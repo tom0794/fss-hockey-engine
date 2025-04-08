c72a5c14070b454debcadc78bb2e1d39b564f7ec
Tom Chiasson <66444452+tom0794@users.noreply.github.com>
2025-04-07 21:01:39 -0400

    Allow cross origin to work with frontend (#3)

---
+++ fss-hockey-spring/src/main/java/io/github/tom0794/WebConfig.java
@@ -0,0 +1,27 @@
+package io.github.tom0794;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins(
+                                "http://localhost:5173",
+                                System.getenv("FRONTEND_URL") != null ? System.getenv("FRONTEND_URL") : "http://host.docker.internal:8082"
+                        )
+                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                        .allowedHeaders("*")
+                        .allowCredentials(true);
+            }
+        };
+    }
+}
+
